83293d73c922dca4ce7f24e011e810c34c3f3b15
Студенческая <@rbiu.local>
2024-10-14 13:00:03 +0500

    brezze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MainController;
-use App\Http\Controllers\StudentController;
- 
 
-Route::get('/', [MainController::class, 'show'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/first', [MainController::class, 'showw'])->name('first');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
-
-Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-
-Route::get('/students/{student}',[StudentController::class, 'showww'])->name('students.showww');
-
-Route::put('/students/{student}',[StudentController::class, 'update'])->name('students.update');
+require __DIR__.'/auth.php';
