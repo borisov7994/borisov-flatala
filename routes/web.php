9a73b81c4aadfd8d4ddbc65443c724fb560a3bd6
Студенческая <@rbiu.local>
2024-10-18 12:53:41 +0500

    connection brezze and create auth

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\StudentController;
+ 
+
+Route::get('/', [MainController::class, 'show'])->name('home');
+
+Route::get('/first', [MainController::class, 'showw'])->name('first');
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,6 +21,16 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+    Route::get('/students/{student}',[StudentController::class, 'showww'])->name('students.showww');
+
+    Route::put('/students/{student}',[StudentController::class, 'update'])->name('students.update');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
