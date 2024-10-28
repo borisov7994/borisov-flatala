126c9c2ecdbee22588f90ef557ab3a36138e487a
Студенческая <@rbiu.local>
2024-10-28 12:57:34 +0500

    28.10.2024 create role admin

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\StudentController;
- 
+use App\Http\Middleware\Admin;
 
 Route::get('/', [MainController::class, 'show'])->name('home');
 
@@ -19,7 +19,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    
+
+    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 
@@ -34,5 +36,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(Admin::class)->group(
+    function() {
+        Route::get('/admin', function(){
+    return view('admin.index');
+})->name('admin.index');
+    }
+);
+
 
 require __DIR__.'/auth.php';
